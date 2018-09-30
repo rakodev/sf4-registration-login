32dcd0f3e117c89841f9fba5a5d37680c90a3b1f
Ramazan KORKMAZ <neoramax@gmail.com>
2018-09-30 21:18:08 +0300

    Some unused method deleted

--- Projects/App/src/Repository/User/UserInfoRepository.php
+++ Projects/App/src/Repository/User/UserInfoRepository.php
@@ -4,7 +4,6 @@ namespace App\Repository\User;
 
 
 use App\Entity\User\UserInfo;
-//use App\Repository\Base\BaseRepository;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
@@ -14,41 +13,4 @@ class UserInfoRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, UserInfo::class);
     }
-
-    public function addOrUpdateLanguage($userId, $language)
-    {
-        if (empty($entity)) {
-            /** @var UserInfo $entity */
-            $entity = $this->findOneBy(['userId' => $userId]);
-        }
-        if(empty($entity)) {
-            $entity = new UserInfo();
-            $entity->setUserId($userId);
-        }
-        $entity->setUserLanguage($language);
-
-        $this->em->persist($entity);
-        $this->em->flush();
-
-        $this->getUserInfo($userId, true);
-    }
-
-    public function getUserInfo($userId, $clearCache = false)
-    {
-        $parameters = ['userId' => $userId];
-        $query = "  SELECT *
-                    from ".$this->getTableName()."
-                    where user_id = :userId
-                    limit 1";
-        $result = $this->getCacheContent($query, $parameters, $clearCache);
-        if(empty($result)) {
-            $stmt = $this->conn->prepare($query);
-            $stmt->execute($parameters); // give parameter in SQL query like this :price
-            $result = $stmt->fetch();
-            $this->setCacheContent($query, $parameters, $result, $this->cacheService->getTimeoutLong());
-        }
-
-        return $result;
-    }
-
 }
--- Projects/App/src/Repository/User/UserRepository.php
+++ Projects/App/src/Repository/User/UserRepository.php
@@ -3,12 +3,10 @@
 namespace App\Repository\User;
 
 use App\Entity\User\User;
-//use App\Repository\Base\BaseRepository;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class UserRepository extends ServiceEntityRepository implements UserLoaderInterface
 {
@@ -25,26 +23,6 @@ class UserRepository extends ServiceEntityRepository implements UserLoaderInterf
     public function loadUserById($userId)
     {
         return $this->findOneById($userId);
-    }
-
-    public function updatePassword($userId, $password)
-    {
-        $user = $this->findOneById($userId);
-        $user->setPassword($password);
-        $this->_em->persist($user);
-        $this->_em->flush();
-        $cacheKey = $this->getCacheKey('user_' .$userId);
-        $this->cache->delete($cacheKey);
-        return $user;
-    }
-
-    public function isUsernameValid($email)
-    {
-        $user = $this->findOneBy(['username' => $email]);
-        if(empty($user)) {
-            return false;
-        }
-        return true;
     }
 
 }
